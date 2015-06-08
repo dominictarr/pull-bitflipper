22646ffb18c7e39353a0b159a1c1b34faa4fe232
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-08 13:30:17 +0200

    support a counter until the packet which will be tampered with

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 var pull = require('pull-stream')
 
 module.exports = function (prob) {
+  
   return pull.map(function (data) {
 
-    if(Math.random() < prob) {
+    if(Number.isInteger(prob) && --prob===0 || Math.random() < prob) {
       var rbit = 1<<(8*Math.random())
       var i = ~~(Math.random()*data.length)
       data[i] = data[i]^rbit
